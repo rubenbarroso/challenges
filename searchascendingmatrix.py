1a173387f8eb6e8da63d2705c2ed9675fc79ab00
Ruben Barroso <ruben@txvia.com>
2012-02-15 10:01:32 +0100

    Formatting

--- searchascendingmatrix.py
+++ searchascendingmatrix.py
@@ -7,23 +7,28 @@ February 10, 2012
 
 Today’s exercise is taken from our inexhaustible list of interview questions:
 
-Given an m by n matrix of integers with each row and column in ascending order, search the matrix and find the row and column where a key k appears, or report that k is not in the matrix. For instance, in the matrix
+Given an m by n matrix of integers with each row and column in ascending order,
+search the matrix and find the row and column where a key k appears, or report
+that k is not in the matrix. For instance, in the matrix
 
  1  5  7  9
  4  6 10 15
  8 11 12 19
 14 16 18 21
 
-the key 11 appears in row 2, column 1 (indexing from 0) and the key 13 is not present in the matrix. The obvious algorithm takes time O(m x n) to search the matrix row-by-row, but you must exploit the order in the matrix to find an algorithm that takes time O(m + n).
+the key 11 appears in row 2, column 1 (indexing from 0) and the key 13 is not
+present in the matrix. The obvious algorithm takes time O(m x n) to search the
+matrix row-by-row, but you must exploit the order in the matrix to find an
+algorithm that takes time O(m + n).
 
 Your task is to write the requested search function.
 """
 
 def search(k, matrix):
-    """ We loop over the diagonal elements looking for the first one greather than the number.
-        Then, we carry out a linear search over the elements from the previous diagonal element
-        -what we refer to as pivot- and the current one. It is easily seen that this algorithm
-        is O(m + n). """
+    """ We loop over the diagonal elements looking for the first one greather
+        than the number.Then, we carry out a linear search over the elements
+        from the previous diagonal element -what we refer to as pivot- and
+        the current one. It is easily seen that this algorithm is O(m + n). """
 
     def linear_search(k, list):
         for i in range(len(list)):
